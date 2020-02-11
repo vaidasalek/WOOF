03f234f5d454cb6386b8319190f1900133f48133
vaidasalek <57872113+vaidasalek@users.noreply.github.com>
2020-02-12 00:12:16 +0200

    +Atsiliepimai, Archyvas

---
+++ root/assets/js/custom.js
@@ -0,0 +1,19 @@
+// When the DOM is ready, run this function
+$(document).ready(
+function() {
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
+
+});
+
+//Archive
+  $(document).ready(function() {
+    $('#myCarousel').carousel({
+      interval: 6000,
+    });
+  });
+$(function () {
+$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
