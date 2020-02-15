ec76aa29254b918ab3fbad7a20840bba318fda8a
vaidasalek <57872113+vaidasalek@users.noreply.github.com>
2020-02-15 17:16:43 +0200

    xz

--- root/assets/js/custom.js
+++ root/assets/js/custom.js
@@ -9,11 +9,22 @@ function() {
 });
 
 //Archive
-  $(document).ready(function() {
-    $('#myCarousel').carousel({
-      interval: 6000,
-    });
-  });
-$(function () {
-$("#mdb-lightbox-ui").load("assets/mdb-addons/mdb-lightbox-ui.html");
-});
+// var modal = document.getElementById("myModal");
+//
+//// Get the image and insert it inside the modal - use its "alt" text as a caption
+//var img = document.getElementById("myImg");
+//var modalImg = document.getElementById("img01");
+//var captionText = document.getElementById("caption");
+//img.onclick = function(){
+//  modal.style.display = "block";
+//  modalImg.src = this.src;
+//  captionText.innerHTML = this.alt;
+//}
+//
+//// Get the <span> element that closes the modal
+//var span = document.getElementsByClassName("close")[0];
+//
+//// When the user clicks on <span> (x), close the modal
+//span.onclick = function() {
+//  modal.style.display = "none";
+//}
